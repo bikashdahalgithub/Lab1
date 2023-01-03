e68ea326d42a54b6067ce784ee4fc6182ef7dc84
bikashdahalgithub <bikashdahal991@gmail.com>
2023-01-03 10:52:16 +0545

    Create an example of an if statement that uses a code block

--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
 ﻿bool flag = true;
-int value = 0;
-
 if (flag)
 {
-    value = 10;
-    Console.WriteLine("Inside of code block: " + value);
+    Console.WriteLine(flag);
 }
-Console.WriteLine("Outside of code block: " + value);
